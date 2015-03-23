1d7e5f89c62f05ee1e8dd32937f3f99a7c5d77a0
beka <beka@vobi.ge>
2015-03-23 22:14:15 +0400

    lean_mongoose

---
+++ learn_mongoose/index.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Cat = mongoose.model('Cat', 
+	{ name: String }
+	);
+
+var kitty = new Cat({ name: 'Zildjian' });
+
+
+kitty.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
+
+console.log("..........................")
